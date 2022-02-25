1606aa7332b115de180efa96ae8a4b9d7dbf87ac
hominglesi <creepersheep1010@gmail.com>
2022-02-25 17:36:09 +0100

    Progressed

--- p/beta/src/App.svelte
+++ p/beta/src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	export let name;
+	import Code from './Code.svelte';
+	import Page from './Page.svelte';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Code />
+	<Page />
 </main>
 
 <style>
