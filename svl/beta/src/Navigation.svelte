a8ebdb89baf8e7985ad0591edb796716013817bd
DamboSlonce <100431100+DamboSlonce@users.noreply.github.com>
2022-02-26 16:35:25 +0100

    Added DDL component

---
+++ svl/beta/src/Navigation.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let link;
+    export let name;
+    export let isDropDownList=false;
+
+</script>
+
+{#if isDropDownList}
+    <div class="ddl">
+        <slot></slot>
+    </div>
+{/if}
+<div class="nddl" on:click = {window.location.href = link}>
+    {name}
+</div>
+
+<style>
+    .ddl{
+        display: none;
+        height: 40px;
+        width: 150px;
+        text-align: center;
+        color: white;
+    }
+    .ddl:hover{
+        display: block;
+    }
+    .nddl{
+        color: white;
+        height: 40px;
+        width: 150px;
+        text-align: center;
+    }
+    .nddl:hover{
+        background-color: white;
+        color: black;
+    }
+    .nddl:hover .ddl{
+        display: block;
+    }
+</style>
