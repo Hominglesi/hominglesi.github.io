9300e7bf9f9aa97c2751675aa58a70db93195be2
hominglesi <creepersheep1010@gmail.com>
2022-02-26 15:11:49 +0100

    Create Container.svelte

---
+++ svl/beta/src/Container.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let width = "100%";
+    export let height = "100%";
+    export let flexDirection = "row";
+
+
+</script>
+
+<style>
+    div{
+        display: flex;
+        
+    }
+
+</style>
+
+<div style="width: {width}; height:{height}; flex-direction:{flexDirection};"></div>
