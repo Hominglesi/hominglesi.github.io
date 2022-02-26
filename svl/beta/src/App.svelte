ae4f782e93008c987a5a3f7dfc2cee58486d7e24
DamboSlonce <100431100+DamboSlonce@users.noreply.github.com>
2022-02-26 15:16:20 +0100

    Added side button

--- svl/beta/src/App.svelte
+++ svl/beta/src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	import SideButton from "./SideButton.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>After copying the template remember to change the build directory in package.json to the desired directory</p>
+<SideButton/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ svl/beta/src/SideButton.svelte
@@ -0,0 +1,20 @@
+<script>
+    let name;
+</script>
+
+
+<button>{name}</button>
+
+
+<style>
+    button{
+        border: 2px solid black;
+        width: 350px;
+        padding-top: 8px;
+        padding-bottom: 8px;
+    }
+    button:hover{
+        background-color: black;
+        color: white;
+    }
+</style>
